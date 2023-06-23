3c529f1d3afba7063c4ebf74b09b13b9b2830e43
seulgi <guseul9405@gmail.com>
2023-06-23 23:18:18 +0900

    갯수 변경 추가

--- app/cart/page.js
+++ app/cart/page.js
@@ -2,19 +2,48 @@ import Link from 'next/link'
 import React from 'react'
 
 export default function Cart() {
+  let 장바구니 = ['Tomatoes', 'Pasta']
+  let 장바구니2 = [
+    {
+      itemName : 'Tomatoes',
+      price : '$40',
+      count : 2,
+    },
+    {
+      itemName : 'Pasta',
+      price : '$10',
+      count : 5,
+    },
+  ]
+
   return (
     <div>
       <h4 className="title">Cart</h4>
-      <div className="cart-item">
-        <p>상품명</p>
-        <p>$40</p>
-        <p>1개</p>
-      </div>
-      <div className="cart-item">
-        <p>상품명</p>
-        <p>$40</p>
-        <p>1개</p>
-      </div>
+      {장바구니2.map((el, idx) => {
+        return (
+          <div key={idx}>
+            <CartItem {...el}  />
+            <Btn color='red' />
+          </div>
+        )
+      })}
+
     </div>
   )
 } 
+
+function CartItem(props) {
+  return (
+    <div className="cart-item">
+      <p>{props.itemName}</p>
+      <p>{props.price}</p>
+      <p>{props.count}개</p>
+    </div>
+  )
+}
+
+function Btn({color}) {
+  return (
+    <button style={{ backgroundColor : color}}>버튼</button>
+  )
+}
--- app/list/page.js
+++ app/list/page.js
@@ -1,21 +1,38 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
 import Image from 'next/image'
 import food0 from '/public/food0.png'
 import food1 from '/public/food1.png'
 import food2 from '/public/food2.png'
 
 export default function List() {
+  const [cnt, setCnt] = useState([1, 1, 1]);
+
   let 상품 = ['Tomatoes', 'Pasta', 'Coconut']
   let 상품이미지 = [food0, food1, food2]
   return (
     <div>
       <h2 className='title'>Products</h2>
       {상품.map((el, idx) => {
-        return (<div className='food' key={idx}>
-          {/* <img src={`/food${idx}.png`} alt='상품 이미지' className='food-img' /> */}
-          <Image src={상품이미지[idx]} alt='상품 이미지' className='food-img' />
-          <h4>{el}</h4>
-        </div>)
+        return (
+          <div className='food' key={idx}>
+            {/* <img src={`/food${idx}.png`} alt='상품 이미지' className='food-img' /> */}
+            <Image src={상품이미지[idx]} alt='상품 이미지' className='food-img' />
+            <h4>{el}</h4>
+            <button onClick={() => {
+              let origin = [...cnt];
+              origin[idx]--;
+              setCnt(origin);
+            }}>-</button>
+            <span> {cnt[idx]} </span>
+            <button onClick={() => {
+              let origin = [...cnt];
+              origin[idx]++;
+              setCnt(origin);
+              }}>+</button>
+          </div>
+        )
       })}
     </div>
   )
